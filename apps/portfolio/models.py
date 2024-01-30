e7ec5b8a9f70aa94b4678b5f708ce23b6d85892c
cailtondev <casiooliveyracosta@gmail.com>
2024-01-30 16:13:48 -0300

    perf: New PostgreSQL database

--- apps/portfolio/models.py
+++ apps/portfolio/models.py
@@ -6,17 +6,11 @@ class Home(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.title
-
 
 class WebDevelopmentOffer(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     paragraph = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.title
 
 
 class WebDevelopmentCard(models.Model):
@@ -25,17 +19,11 @@ class WebDevelopmentCard(models.Model):
     card_title = models.CharField(max_length=150)
     card_paragraph = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.card_title
-
 
 class WebDevelopmentTechnologies(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
     icon_link = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.title
 
 
 class WebDevelopmentProjects(models.Model):
@@ -46,9 +34,6 @@ class WebDevelopmentProjects(models.Model):
     image_link = models.CharField(max_length=255)
     alt_image = models.CharField(max_length=100)
     link_preview = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.title
 
 
 class ResumeME(models.Model):
@@ -61,6 +46,6 @@ class Contact(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=30)
     paragraph = models.CharField(max_length=200)
-    email = models.CharField(max_length=150)
-    phone = models.CharField(max_length=10)
+    email = models.CharField(max_length=250)
+    phone = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
