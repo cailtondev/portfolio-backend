566d9beabf404033754868c200ff49974abbcdf6
cailtondev <casiooliveyracosta@gmail.com>
2024-02-06 15:30:15 -0300

    fix: Solution for the database problem

--- apps/portfolio/models.py
+++ apps/portfolio/models.py
@@ -6,16 +6,21 @@ class Home(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.title
+
 
 class WebDevelopmentOffer(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     paragraph = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.title
+
 
 class WebDevelopmentCard(models.Model):
     id = models.AutoField(primary_key=True)
-    offer = models.ForeignKey(WebDevelopmentOffer, on_delete=models.CASCADE)
     card_title = models.CharField(max_length=150)
     card_paragraph = models.CharField(max_length=100)
 
@@ -24,6 +29,9 @@ class WebDevelopmentTechnologies(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
     icon_link = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
 
 
 class WebDevelopmentProjects(models.Model):
@@ -35,11 +43,17 @@ class WebDevelopmentProjects(models.Model):
     alt_image = models.CharField(max_length=100)
     link_preview = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.title
+
 
 class ResumeME(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=80)
     paragraph = models.TextField()
+
+    def __str__(self):
+        return self.title
 
 
 class Contact(models.Model):
@@ -49,3 +63,6 @@ class Contact(models.Model):
     email = models.CharField(max_length=250)
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
